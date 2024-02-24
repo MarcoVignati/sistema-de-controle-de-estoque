b546a09082cee9c1839ac12163275f7d6ad91425
Marco Vignati <marcovignatidev@gmail.com>
2024-02-24 18:07:33 -0400

    create service methods for FornecedorService

--- src/main/java/com/marcovignati/controledeestoque/service/FornecedorService.java
+++ src/main/java/com/marcovignati/controledeestoque/service/FornecedorService.java
@@ -1,8 +1,12 @@
 package com.marcovignati.controledeestoque.service;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.marcovignati.controledeestoque.model.Fornecedor;
 import com.marcovignati.controledeestoque.repository.FornecedorRepository;
 
 @Service
@@ -11,4 +15,30 @@ public class FornecedorService {
   @Autowired
   private FornecedorRepository fornecedorRepository;
 
+  public Fornecedor salvarFornecedor(Fornecedor fornecedor) {
+    return fornecedorRepository.save(fornecedor);
+  }
+
+  public List<Fornecedor> listarFornecedores() {
+    return fornecedorRepository.findAll();
+  }
+
+  public Fornecedor buscarFornecedorPorId(UUID id) {
+    return fornecedorRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Fornecedor não encontrado"));
+  }
+
+  public Fornecedor atualizarFornecedor(UUID id, Fornecedor fornecedor) {
+    Fornecedor fornecedorExistente = buscarFornecedorPorId(id);
+    fornecedorExistente.setNome(fornecedor.getNome());
+    fornecedorExistente.setEndereco(fornecedor.getEndereco());
+    fornecedorExistente.setTelefone(fornecedor.getTelefone());
+    fornecedorExistente.setEmail(fornecedor.getEmail());
+    return fornecedorRepository.save(fornecedorExistente);
+  }
+
+  public void excluirFornecedor(UUID id) {
+    Fornecedor fornecedorExistente = buscarFornecedorPorId(id);
+    fornecedorRepository.delete(fornecedorExistente);
+  }
 }
